48d828b10d44d015878fd58e8e1b528e193223cf
Nick Porter <nick@porternick.com>
2015-04-25 22:01:41 +0000

    Adds static pages and basic navigation between them

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home' => 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
