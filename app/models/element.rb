0796a2c9a634a188b83d25a03bb6892c6f288258
Nick Porter <nick@porternick.com>
2015-06-13 15:39:36 -0600

    Adds evidence upload to element

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,8 +1,10 @@
 class Element < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :evidence, :styles => { :large => "700x700>"}, :default_url => "/images/:style/missingevidence.png"
+  validates_attachment_content_type :evidence, :content_type => /\Aimage\/.*\Z/
 
   def self.standards
     ['Patterns', 'Cause and effect', 'Scale, proportion, and quantity', 'Energy and matter', 'Structure and function', 'Stability and change']
   end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518180552) do
+ActiveRecord::Schema.define(version: 20150612214136) do
 
   create_table "elements", force: :cascade do |t|
     t.string   "title"
-    t.string   "evidence"
     t.string   "standard"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.integer  "user_id"
+    t.string   "evidence_file_name"
+    t.string   "evidence_content_type"
+    t.integer  "evidence_file_size"
+    t.datetime "evidence_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
